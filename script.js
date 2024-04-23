9ee87aa91e27c8d32ae22029b341caae38b1fb79
Raniery Mendes <54686276+RanieryMendes@users.noreply.github.com>
2024-04-22 23:06:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,44 +23,89 @@ function get_fields(){
     }
 
 }
-
-function convert(){
-
-  
-
-    if(get_fields() == false){
-       //do nothing just the alert
-       
+function convert() {
+    if (get_fields() == false) {
+        // If get_fields() returns false, simply return to stop execution.
+        return;
     }
 
-    else{
+    // Clear previous response
+    document.getElementById('response').innerHTML = '';
 
-      document.getElementById('response').innerHTML = ''
+    // Ensure variables are defined; assuming these are obtained from form inputs
+    var addressLink = document.getElementById('address_link').value;
+    var slashLink = document.getElementById('slash_link').value;
+    var linkTitle = document.getElementById('tittle_link').value; // Check if the ID is correct, might be 'title_link'
 
     fetch("https://api.rebrandly.com/v1/links", {
-  "method": "POST",
-  "headers": {
-    "Content-Type": "application/json",
-    "apikey": "e70f2fb1499d49d4a76c5512b7b8ef1e"
-  },
-  "body": `{\"destination\":\"${addressLink}\",\"slashtag\":\"${slashLink}\",\"title\":\"${linkTittle}\"}`
-})
-.then(response => {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "apiKey": "e70f2fb1499d49d4a76c5512b7b8ef1e"  // Check API documentation for exact key name
+        },
+        body: JSON.stringify({
+            destination: addressLink,
+            slashtag: slashLink,
+            title: linkTitle
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        document.getElementById('response').innerHTML = `<p class="mt-5 p-2">Here you are: ${data.title} <a href="https://${data.shortUrl}" target="_blank">${data.shortUrl}</a></p>`;
+        // Reset form fields
+        document.getElementById('tittle_link').value = '';
+        document.getElementById('slash_link').value = '';
+        document.getElementById('address_link').value = '';
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        document.getElementById('response').innerHTML = `<p class="mt-5 p-2 text-danger">Error: ${error.message}</p>`;
+    });
+}
+
+// function convert(){
+
   
-  return response.json().then(data=>{
-    console.log(data)
 
-    document.getElementById('response').innerHTML = `<p class="mt-5 p-2">  Here you are: ${data.title} <a href="https://www.${data.shortUrl}" target= "_blank"> ${data.shortUrl}</a> </p>`
+//     if(get_fields() == false){
+//        //do nothing just the alert
+       
+//     }
 
-   document.getElementById('tittle_link').value = ''
+//     else{
 
-   document.getElementById('slash_link').value = ''
+//       document.getElementById('response').innerHTML = ''
 
-   document.getElementById('address_link').value = ''
+//     fetch("https://api.rebrandly.com/v1/links", {
+//   "method": "POST",
+//   "headers": {
+//     "Content-Type": "application/json",
+//     "apikey": "e70f2fb1499d49d4a76c5512b7b8ef1e"
+//   },
+//   "body": `{\"destination\":\"${addressLink}\",\"slashtag\":\"${slashLink}\",\"title\":\"${linkTittle}\"}`
+// })
+// .then(response => {
+  
+//   return response.json().then(data=>{
+//     console.log(data)
+
+//     document.getElementById('response').innerHTML = `<p class="mt-5 p-2">  Here you are: ${data.title} <a href="https://www.${data.shortUrl}" target= "_blank"> ${data.shortUrl}</a> </p>`
+
+//    document.getElementById('tittle_link').value = ''
+
+//    document.getElementById('slash_link').value = ''
+
+//    document.getElementById('address_link').value = ''
 
 
 
-  })
+//   })
 
 
 })
